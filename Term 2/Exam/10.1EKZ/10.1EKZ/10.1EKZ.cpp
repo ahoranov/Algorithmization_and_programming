62b2e272c718707a5cdaea770c86c53d5bfdf06a
hooranov <hooranov@gmail.com>
2024-02-18 15:44:54 +0200

    first commit

---
+++ Term 2/Exam/10.1EKZ/10.1EKZ/10.1EKZ.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+bool search(const char* s);
+
+int main()
+{
+	char str[101];
+
+	ifstream f("test.txt");
+	f >> str;
+	if (search(str))
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	f.close();
+
+	return 0;
+}
+
+bool search(const char* s)
+{
+	for (int j = 0; s[j] != '\0'; j++)
+		if ((s[j] == 'a' && s[j + 1] == 'a') || (s[j] == 'b' && s[j + 1] == 'b') || (s[j] == 'c' && s[j + 1] == 'c'))
+			return true;
+	return false;
+}
---
+++ Term 2/Exam/8.2EKZ/8.2EKZ/8.2EKZ.cpp
@@ -0,0 +1,31 @@
+﻿#include <string.h>
+#include <iostream>
+
+using namespace std;
+bool FirstLetter(char* str);
+
+int main()
+{
+    char s[101];
+    cout << "Enter string:" << endl;
+    cin.getline(s, 101);
+    if (FirstLetter(s))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+bool FirstLetter(char* str)
+{
+    char* nextToken;
+    char* str2 = strtok_s(str, " ", &nextToken);
+    while (str2 != NULL)
+    {
+        if (str2[0] == 'a')
+            return true;
+
+        str2 = strtok_s(NULL, " ", &nextToken);
+    }
+
+    return false;
+}
